1ff112055e54e45fc2929266c6700ab333642cd2
Natalia Alvarez <nataliamicaela26@hotmail.com>
2026-07-01 15:05:11 -0300

    Estructura inicial del proyecto con React Router

---
+++ src/routes/AppRouter.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
+import Tasks from "../pages/Tasks/Tasks";
+
+const AppRouter = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/tasks" element={<Tasks />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
+export default AppRouter;
