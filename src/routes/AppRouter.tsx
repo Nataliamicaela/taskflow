4006f77c15e99458dd3752013e1def381ca0ed7f
Natalia Alvarez <nataliamicaela26@hotmail.com>
2026-07-01 21:53:43 -0300

    Completar autenticación y proteger rutas privadas

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -5,14 +5,26 @@ import Login from "../pages/Login/Login";
 import Register from "../pages/Register/Register";
 import Tasks from "../pages/Tasks/Tasks";
 
+import ProtectedRoute from "./ProtectedRoute";
+
 const AppRouter = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
+
                 <Route path="/login" element={<Login />} />
+
                 <Route path="/register" element={<Register />} />
-                <Route path="/tasks" element={<Tasks />} />
+
+                <Route
+                    path="/tasks"
+                    element={
+                        <ProtectedRoute>
+                            <Tasks />
+                        </ProtectedRoute>
+                    }
+                />
             </Routes>
         </BrowserRouter>
     );
